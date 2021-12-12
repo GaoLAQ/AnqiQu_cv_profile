0d09a97be0d90b63dc4773b1ff251479dc1cf873
HAOXIANG GAO <dashen1106@gmail.com>
2021-12-12 14:35:28 +0000

    Change introduction note

--- cv-profile/src/components/Card.vue
+++ cv-profile/src/components/Card.vue
@@ -4,19 +4,17 @@
       <v-card-title
         class="bg-news d-flex justify-start text-justify text-xs-h3 text-md-h4 text-lg-h4 text-xl-h4 white--text pa-4"
       >
-        Music Composer
+        Goals
       </v-card-title>
       <v-card-text>
         <v-list-item class="mt-4">
           <p
             class="d-flex justify-start text-justify text-xs-h3 text-md-h5 text-lg-h5 text-xl-h5 white--text pa-1"
           >
-            Born in Italy and now resident in London, I was a concert pianist
-            and composer once; fascinated by screen music, I have been
-            dedicating my soul to this art since 2006. Yes, I won awards, yes, I
-            have been involved in top productions and built a solid career. But
-            ultimately I create inspiring musical worlds for stories that
-            inspire me in return.
+            Born in China and now resident in London, Anqi was a music graduated
+            student and willing to become a composer in future; fascinated by
+            screen music, She want to bring its dynamic passion to create
+            inspiring musical worlds
           </p>
         </v-list-item>
       </v-card-text>
