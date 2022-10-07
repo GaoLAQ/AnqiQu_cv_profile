c2855b276ab69931c17843aa7c992ee7c82d1a46
dashen <dashen1106@gmail.com>
2022-10-07 22:56:58 +0100

    add videos store

--- cv-profile/src/store/index.js
+++ cv-profile/src/store/index.js
@@ -27,6 +27,28 @@ export default new Vuex.Store({
       configContent:
         "I was a music graduated student and willing to become a composer in future; fascinated by screen music, She want to bring her dynamic passion to create inspiring musical worlds. She is now in London.",
     },
+    videoDataBase: [
+      {
+        description:
+          "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
+        source:
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        title: "Big Buck Bunny",
+      },
+      {
+        description: "The first Blender Open Movie from 2006",
+        source:
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+        title: "Elephant Dream",
+      },
+      {
+        description:
+          "HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.",
+        source:
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+        title: "For Bigger Blazes",
+      },
+    ],
   },
   mutations: {},
   actions: {},
--- cv-profile/src/views/Videos.vue
+++ cv-profile/src/views/Videos.vue
@@ -5,7 +5,7 @@
         class="rounded-0 mb-2"
         dark
         width="500px"
-        v-for="video in videoDataBase"
+        v-for="video in videos"
         :key="video.source"
       >
         <video id="myPlayer" width="500px" controls>
@@ -20,35 +20,17 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   data: () => {
-    return {
-      videoDataBase: [
-        {
-          description:
-            "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
-          source:
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-          title: "Big Buck Bunny",
-        },
-        {
-          description: "The first Blender Open Movie from 2006",
-          source:
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-          title: "Elephant Dream",
-        },
-        {
-          description:
-            "HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.",
-          source:
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-          title: "For Bigger Blazes",
-        },
-      ],
-    };
+    return {};
   },
   created() {},
-  computed: {},
+  computed: {
+    ...mapState({
+      videos: (state) => state.videoDataBase,
+    }),
+  },
   methods: {},
   watch: {},
 };
